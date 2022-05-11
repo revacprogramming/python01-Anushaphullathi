0b29bdd2f2762be00773d6bc6123dc824821b9ad
Anushaphullathi <96220359+Anushaphullathi@users.noreply.github.com>
2022-05-11 10:35:29 +0000

    var,exp,stmts

--- ActivitySet01/problem03.py
+++ ActivitySet01/problem03.py
@@ -1,9 +1,5 @@
 # Variables, Expressions & Statements
-hrs=input("Enter hours ")
-h=float(hrs)
-rp=input("Enter rate per hour ")
-x=float(rp)
-if float(hrs)<40:
-	print(h*x)
-elif float(hrs)>40:
-	print(40* x + (h-40)*1.5*x)
+hrs = input("Enter Hours:")
+rt=input("Enter Rate:")
+p=float(hrs)*float(rt)
+print("Pay:",p)
