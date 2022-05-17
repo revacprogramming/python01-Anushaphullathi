3942705806363373b3aa80a756a4d89e65b1a163
Anushaphullathi <96220359+Anushaphullathi@users.noreply.github.com>
2022-05-17 16:34:07 +0000

    files

--- ActivitySet01/extra.py
+++ ActivitySet01/extra.py
@@ -1,8 +1,12 @@
-a=12
-x=()
-y=()
-x>>2
-y<<1
-print(x,y)
+fname=input("Enter the file name: ")
+fhand=open(fname)
+count=0
+if line in fhand:
+  if line.startswith("Subject:"):
+      count=count+1
+  print("The no. of words are ",count,"In file ",fname)
+  python ActivitySet01/extra.py
+
+
 
 
--- ActivitySet01/problem08.py
+++ ActivitySet01/problem08.py
@@ -1,3 +1,27 @@
 # Files
-
 filename = "dataset/mbox-short.txt"
+name=input("Enter the file name: ")
+fhand=open(name)
+xfile=fhand.read()
+for line in xfile:
+    l=xfile.rstrip()
+    if not l.startswith("X-DSPAM-Confidence:    0.8475"):
+        continue
+    else:
+        count=0
+        starting_point=0
+        count=count+1
+        staring_point=l.find("0")
+        floating_point=float(l[starting_point:])
+        total=starting_point+floating_point
+average=total/count
+print("Average spam confidence: ",average)
+      
+        
+       
+    
+      
+
+
+
+
