2a958a2a5e1c6155c0d33ee1200b2b0a6bce113b
Anushaphullathi <96220359+Anushaphullathi@users.noreply.github.com>
2022-05-17 17:38:45 +0000

    file#

--- ActivitySet01/problem08.py
+++ ActivitySet01/problem08.py
@@ -1,21 +1,20 @@
 # Files
-filename = "dataset/mbox-short.txt"
+# Use the file name mbox-short.txt as the file name
+#filename = "dataset/mbox-short.txt"
 name=input("Enter the file name: ")
 fhand=open(name)
-xfile=fhand.read()
-for line in xfile:
-    l=xfile.rstrip()
-    if not l.startswith("X-DSPAM-Confidence:    0.8475"):
+total=0
+count=0
+for line in fhand:
+    if not line.startswith("X-DSPAM-Confidence:"):
         continue
-    else:
-        count=0
-        starting_point=0
-        count=count+1
-        staring_point=l.find("0")
-        floating_point=float(l[starting_point:])
-        total=starting_point+floating_point
+    x=line.find("0")
+    y=float(line[x:])
+    count=count+1
+    total=total+y
+    
 average=total/count
-print("Average spam confidence: ",average)
+print("Average spam confidence:",average)
       
         
        
