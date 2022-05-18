cc49008461c56fe9b20aba2520ad5c8b4bfb9c9d
Anushaphullathi <96220359+Anushaphullathi@users.noreply.github.com>
2022-05-18 14:54:04 +0000

    ltsort

--- ActivitySet01/problem09.py
+++ ActivitySet01/problem09.py
@@ -1,3 +1,14 @@
 # Lists
 
 filename = "dataset/romeo.txt"
+fname = input("Enter file name: ")
+fh = open(fname)
+lst = list()
+for line in fh:
+    line=line.strip()
+    words=line.split()
+    for nword in words:
+        if not nword in lst:
+            lst.append(nword)       
+lst.sort()
+print(lst)
