23cf2c4499ad3ad0c48c477d74a20bf0e1e7ad7b
Anushaphullathi <96220359+Anushaphullathi@users.noreply.github.com>
2022-05-20 16:35:51 +0000

    dic2.done

--- ActivitySet01/problem10.py
+++ ActivitySet01/problem10.py
@@ -11,3 +11,13 @@ for line in handle:
     words=words[1]
     count[words]=count.get(words,0)+1
 print(words, count[words])
+#filename = "dataset/mbox-short.txt"
+#count=dict()
+#name = input("Enter the file name:")
+#handle = open(name,'r')
+#for line in handle:
+ #   if line.startswith('From '): 
+#    words=line.split()
+#    words=words[1]
+#    count[words]=count.get(words,0)+1
+#print(words, count[words])
